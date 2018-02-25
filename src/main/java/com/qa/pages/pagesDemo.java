741420dc74b832818ebd0009a10f4661c1386089
Divya <divyagvarma@gmail.com>
2018-02-24 19:23:17 -0500

    added more files

--- src/main/java/com/qa/pages/pagesDemo.java
+++ src/main/java/com/qa/pages/pagesDemo.java
@@ -1,9 +1,17 @@
 package com.qa.pages;
 
+import org.testng.annotations.Test;
+
 public class pagesDemo {
+	@Test
 	public void pagesMethod()
 	{
 		System.out.println("I am  from the pages class");
 	}
 
+	@Test
+	public void setMethod()
+	{
+		System.out.println("I am  from the pages class");
+	}
 }
